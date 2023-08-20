c1d897166f1aa93e9fa32921e9d4ef78af8f0877
taiyou <hiromacha1116@icloud.com>
2023-08-20 18:25:09 +0900

    complete sidebar css

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar({ onAddNote }) {
+  return (
+    <div className='app-sidebar'>
+      <div className='app-sidebar-header'>
+        <h1>ノート</h1>
+        <button onClick={onAddNote}>追加</button>
+      </div>
+      <div className='app-sidebar-notes'>
+        <div className='app-sidebar-note'>
+          <div className='app-sidebar-title'>
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容</p>
+          <small>最後の修正日:xx/xx</small>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar

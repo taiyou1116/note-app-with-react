ea6508cf2f12ba438de57a38c77625f5b53d5744
taiyou <hiromacha1116@icloud.com>
2023-08-21 16:13:39 +0900

    complete main-no-note css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import "./Main.css"
 
 function Main({ getActiveNote }) {
+
+  if (!getActiveNote)  {
+    return <div className='no-active-note'>ノートが選択されていません</div>
+  }
+
   return (
     <div className='app-main'>
       <div className='app-main-note-edit'>
-        <input type='text' />
-        <textarea id='' placeholder='内容の入力'></textarea>
+        <input type='text' value={getActiveNote.title} />
+        <textarea id='' placeholder='内容の入力' value={getActiveNote.content}></textarea>
       </div>
       <div className='app-main-note-preview'>
        <h1 className='preview-title'>{getActiveNote?.title}</h1>

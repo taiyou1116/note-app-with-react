b0a675cc44806853736adb40efc87d30210f0246
taiyou <hiromacha1116@icloud.com>
2023-08-21 22:46:19 +0900

    save in localstrage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Sidebar from './components/Sidebar'
 import Main from './components/Main'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import uuid from "react-uuid"
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
   const [activeNote, setActiveNote] = useState();
+  
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes])
 
   const onAddNote = () => {
     const newNote = {
